27c33fdc525b5e25e64cc77e5c7a658eab8734ad
rtrydev <r.kulka44@gmail.com>
2022-05-28 05:14:40 +0200

    working slider

--- containers/AuthHelper.tsx
+++ containers/AuthHelper.tsx
@@ -22,7 +22,7 @@ function AuthHelper() {
 
   useEffect(() => {
     if (!isString(token)) return;
-    router.push(config.homepagePath);
+    //router.push(config.homepagePath);
     Cookies.set(config.cookieTokenName, token);
   }, [token]);
 
--- pages/estimationInput/index.tsx
+++ pages/estimationInput/index.tsx
@@ -1,11 +1,28 @@
 import EstimationCard from "@containers/EstimationCard/EstimationCard";
+import Button from "@containers/Button/Button";
+import usePutRequest from "hooks/usePutRequest";
+import { useState } from "react";
+import Container from "@containers/Container/Container";
+import style from "./EstimationInput.module.scss";
 
 const EstimationInput = ({}) => {
+
+    const [estimation, setEstimation] = useState(200);
+
+    const { fetch } = usePutRequest({ pathname: "/api/estimate" });
+
+    const handleSubmit = async () => {
+        await fetch({
+            estimationId: 0,
+            actualValue: 0
+        });
+    };
+
     return (
-        <div>
-            <EstimationCard title="Test" value={100} min={1} max={10000} onChange={() => {}}></EstimationCard>
-            
-        </div>
+        <Container>
+            <EstimationCard title="Test" value={estimation} min={60} max={5000} onChange={setEstimation}></EstimationCard>
+            <Button onClick={handleSubmit}>Prześlij</Button>
+        </Container>
     )
 }
 
