b9e2758773ef0f4bd698a4e682c705b3eba94280
rtrydev <r.kulka44@gmail.com>
2022-05-28 03:39:49 +0200

    hours coloring

--- containers/EstimationItem/EstimationItem.tsx
+++ containers/EstimationItem/EstimationItem.tsx
@@ -1,6 +1,7 @@
 import Card from "@containers/Card/Card";
 import styles from "./EstimationItem.module.scss"
 import Icon from "@containers/Icon/Icon";
+import Container from "@containers/Container/Container";
 
 export interface IEstimationModel {
     title: string;
@@ -9,17 +10,33 @@ export interface IEstimationModel {
 }
 
 const EstimationItem = ({model}:{model:IEstimationModel}) => {
+
+    const convertToHours = (minutes: number) => {
+        let result = minutes < 0 ? Math.ceil(minutes / 60) : Math.floor(minutes / 60);
+        return result;
+    }
+
+    const convertDiffToStr = (result: number, actualResult: number) => {
+        let value = convertToHours(actualResult - result);
+        return value < 0 ? value + "h" : "+" +  value + "h";
+    }
+
+    let sign = convertToHours(model.userResult - model.result) > 0;
+
     return (
+    
         <div>
             <Card className={styles.card}>
-                <div className={styles.estimation}>
-                    <div>
-                        <p>{model.result}</p>
-                        <p>{model.userResult}</p>
+                <Container>
+                    <div className={styles.flex}>
+                        <div>
+                            <p>{convertToHours(model.result) + "h"}</p>
+                            <p className={!sign ? styles.posVal : styles.negVal}>{convertDiffToStr(model.result, model.userResult)}</p>
+                        </div>
+                        <p>{model.title}</p>
+                        <Icon type="clock"></Icon>
                     </div>
-                    <p>{model.title}</p>
-                    <Icon type="clock"></Icon>
-                </div>
+                </Container>
             </Card>
         </div>
     )
