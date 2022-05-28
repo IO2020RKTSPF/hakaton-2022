74c45c476cfed1b407971757eb6ef9142dfbd9bd
rtrydev <r.kulka44@gmail.com>
2022-05-28 03:01:47 +0200

    bigger icons

--- containers/Icon/Icon.tsx
+++ containers/Icon/Icon.tsx
@@ -62,7 +62,7 @@ function Icon({ type }: { type: type }) {
     }
     case "plus": {
       return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fillRule="evenodd" clipRule="evenodd" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12ZM12 18C11.4477 18 11 17.5523 11 17V13H7C6.44772 13 6 12.5523 6 12C6 11.4477 6.44772 11 7 11H11V7C11 6.44772 11.4477 6 12 6C12.5523 6 13 6.44772 13 7V11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H13V17C13 17.5523 12.5523 18 12 18Z" fill="white"/>
         </svg>
 
@@ -70,7 +70,7 @@ function Icon({ type }: { type: type }) {
     }
     case "clock": {
       return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="12" cy="12" r="7" stroke="white" strokeWidth="2"/>
           <path d="M5.96472 3.1363C5.2865 3.31803 4.66807 3.67508 4.17157 4.17157C3.67508 4.66807 3.31803 5.2865 3.1363 5.96472" stroke="white" stroke-width="2" stroke-linecap="round"/>
           <path d="M18.0353 3.1363C18.7135 3.31803 19.3319 3.67508 19.8284 4.17157C20.3249 4.66807 20.682 5.2865 20.8637 5.96472" stroke="white" stroke-width="2" stroke-linecap="round"/>
